40084a5e6672baf6bda95532ec1fd72ad059fc9a
EubertMartinez <martinezeubert08@gmail.com>
2023-04-10 09:43:31 +0800

    create react build app

--- src/App.js
+++ src/App.js
@@ -3,22 +3,93 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <html lang="en">
+      <head>
+        <meta charSet="UTF-8"/>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <title>P-wise</title>
+        <link href='https://unpkg.com/boxicons@2.0.9/css/boxicons.min.css' rel='stylesheet'/>
+        <link rel="stylesheet" href="style.css"/>
+      </head>
+      <div>
+        <header>
+      		<div className="topbar">
+            <nav>
+              <div className="logo">
+                <a href="#"><img src="pwise-green-logo.png" alt="logo"/></a>
+              </div>
+              <ul>
+                <a href="#" className="notification"><i className='bx bx-bell'></i><span className="badge"></span></a>
+
+                <div className="dropdown">
+                  <button className="dropbtn"><i className='bx bx-cog' ></i>
+                    <i className="fa fa-caret-down"></i>
+                  </button>
+                  <div className="dropdown-content">
+                    <a href="#">Settings and Privacy</a>
+                    <a href="#">Switch Account</a>
+                    <a href="#">Log out</a>
+                  </div>
+                </div>
+              </ul>
+            </nav>
+      		</div>
+      	</header>
+
+        <div className="wrapper">
+      		<div className="sidebar">
+      			<ul>
+              <li>
+                <a href="#" id="active">
+                  <i className="bx bx-tachometer"></i>
+                  <div>Dashboard</div>
+                </a>
+              </li>
+        
+              <li>
+                <a href="#">
+                  <i className="bx bx-right-top-arrow-circle"></i>
+                    <div>Expenses</div>
+                </a>
+              </li>
+        
+              <li>
+                <a href="#">
+                  <i className="bx bx-wallet"></i>
+                    <div>Budget</div>
+                </a>
+              </li>
+        
+              <li>
+                <a href="#">
+                  <i className="bx bx-user"></i>
+                    <div>Household Members</div>
+                </a>
+              </li>
+        
+              <li>
+                <a href="#">
+                  <i className="bx bx-file"></i>
+                    <div>Reports</div>
+                </a>
+              </li>
+        
+              </ul>
+      		</div>
+
+          <main>
+      			<div className="content">
+      				<h2>Dashboard</h2>
+      			</div>
+      		</main>
+
+        </div>
+
+        <script src="#"></script>
+      </div>
+
+    </html>
   );
 }
 
